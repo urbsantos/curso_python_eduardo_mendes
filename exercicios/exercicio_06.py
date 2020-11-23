5c68ac3c755bf9b5bd6e9623f752bcf2971ac4e1
Urbano Santos <urbanolopes@gmail.com>
2020-11-23 20:42:53 -0300

    Exercises

---
+++ exercicios/exercicio_06.py
@@ -0,0 +1,11 @@
+# Faça um programa que pergunte o preço de três produtos e informe qual produto
+# você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+
+preco_1 = float(input('Preço 1: '))
+preco_2 = float(input('Preço 2: '))
+preco_3 = float(input('Preço 3: '))
+
+if preco_1 > preco_2 and preco_2 > preco_3:
+    print('Compre o 3')
+elif preco_2 > preco_1 and preco_1 < preco_3:
+    print('Compre o 1')
---
+++ exercicios/exercicio_5.py
@@ -0,0 +1,8 @@
+"""
+Faça um programa para a leitura de duas notas de um aluno.
+O programa deve calcular a média alcançada por aluno e apresentar:
+
+- A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+- A mensagem "Reprovado", se a média for menor do que sete;
+- A mensagem "Aprovado com distinção", se a média for igual a dez.
+"""
